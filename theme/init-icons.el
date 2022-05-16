d7041b6a6bf95ef1a81cebfc8aa0de840f495747
liaochengjun <liaochengjun@ticpsh.com>
2022-05-16 18:07:11 +0800

    comment

--- theme/init-icons.el
+++ theme/init-icons.el
@@ -2,6 +2,15 @@
 :ensure t
 :defer 4)
 
+;; (use-package all-the-icons-ivy
+;; :ensure t
+;;   :after (all-the-icons ivy)
+;;   :custom (all-the-icons-ivy-buffer-commands '(ivy-switch-buffer-other-window ivy-switch-buffer))
+;;   :config
+;;   (add-to-list 'all-the-icons-ivy-file-commands 'counsel-dired-jump)
+;;   (add-to-list 'all-the-icons-ivy-file-commands 'counsel-find-library)
+;;   (all-the-icons-ivy-setup))
+
 
 (use-package all-the-icons-dired
 :ensure t
